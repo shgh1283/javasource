f70f2b2090d1c708ab02b03111c4ead30de3bd1b
shgh1283 <shingh1283@gmail.com>
2024-08-22 17:20:18 +0900

    240822 java

---
+++ Array/Array/ArrayEx3.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class ArrayEx3 {
+    public static void main(String[] args) {
+        // char 배열 선언, 생성
+        // 타입[] 배열명
+        // 타입 배열명[]
+        char[] chArr = { 'a', 'b', 'c', 'd' };
+
+        // float 배열 선언, 생성
+        float[] flArr = { 12.5f, 13.6f, 14.5f, 13.7f };
+
+        // String 배열 선언, 생성
+        String[] strArr = { "A", "B", "C", "D" };
+        System.out.println(strArr[3]);
+    }
+}
