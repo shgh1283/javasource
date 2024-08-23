9094d6a3679947f505baf3df0bfc13e2a0f0ebfc
shgh1283 <shingh1283@gmail.com>
2024-08-23 18:12:08 +0900

    240823 java

--- OBJECT/src/App.java
+++ OBJECT/src/App.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
-=======
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
->>>>>>> ed2c435241f44c6a1c804d573ef7af61a47d6ca4
--- OBJECT/src/classtest/Car.java
+++ OBJECT/src/classtest/Car.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package classtest;
 
 // 자바 - 객체지향언어
@@ -27,33 +26,3 @@ public class Car {
     }
 
 }
-=======
-package classtest;
-
-// 자바 - 객체지향언어
-//        main() 메소드를 가지는 클래스와 가지지 않는 클래스
-
-// 클래스 정의 : 객체를 정의해 놓은 것
-// 클래스 용도 : 객체 생성하는데 사용
-
-// 객체 : 사물 또는 개념
-
-public class Car {
-    // 특징(속성) => 필드(변수로 작성)
-    // 제조사, 모델, 색상, 최대 속도
-    String company;
-    String model;
-    String color;
-    int maxSpeed;
-
-    // 기능(동작) => 메소드
-    void foward() {
-        System.out.println("전진한다.");
-    }
-
-    void backword() {
-        System.out.println("후진한다");
-    }
-
-}
->>>>>>> ed2c435241f44c6a1c804d573ef7af61a47d6ca4
