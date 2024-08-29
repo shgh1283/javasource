03dbb1765512130b38f5437bc48a420d4b237c66
shgh1283 <shingh1283@gmail.com>
2024-08-29 18:00:08 +0900

    240829

---
+++ OBJECT/src/inter/PlayingCard.java
@@ -0,0 +1,27 @@
+package inter;
+
+public interface PlayingCard {
+
+    // 멤버 변수 선언 불가
+    // private String name;
+
+    // 상수
+    public static final int SPADE = 4;
+    // public static final 생략 가능 => 상수만 가능하기 때문
+    int DIAMOND = 3;
+    static int HEART = 2;
+    final int CLOVER = 1;
+
+    // abstract 키워드는 필수가 아님 => 모든 메소드가 추상이기 때문
+    String getCardNumber();
+
+    // 일반 메소드 선언 불가
+    // void method(){}
+
+    // default, static 붙인 메소드는 가능
+    default void method() {
+    }
+
+    static void method2() {
+    }
+}
